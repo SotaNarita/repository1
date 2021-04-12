47e85401d9f03e2c28cb4e83ad9f557c71a936ca
SotaNarita <81950581+SotaNarita@users.noreply.github.com>
2021-04-12 13:11:41 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 for i in range(0, 361, 15):
-    print ("sin" + str(i) + " = " + str(np.math.sin(i)))
-    print ("cos" + str(i) + " = " + str(np.math.cos(i)))
-    print ("tan" + str(i) + " = " + str(np.math.tan(i)) + "\n")
+    print ("sin" + str(i) + "°= " + str(np.sin(np.radians(i))))
+    print ("cos" + str(i) + "°= " + str(np.cos(np.radians(i))))
+    if i == 90 or i == 270:
+        print ("tan" + str(i)+"°は存在しません"+"\n")
+    else:
+        print ("tan" + str(i) + "°= " + str(np.tan(np.radians(i)))+"\n")
